05c2afa9764b0c32dbd64af5bd0fbc27ad770635
cesar.aguilar <Blueavian9@gmail.com>
2022-07-25 15:05:52 -0700

    initial

---
+++ build/precache-manifest.81ed5bb5ade8de79b80633bb5b0247d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6aa4596a286a799c6ee",
+    "url": "/static/css/main.7168a84f.chunk.css"
+  },
+  {
+    "revision": "c6aa4596a286a799c6ee",
+    "url": "/static/js/main.c6aa4596.chunk.js"
+  },
+  {
+    "revision": "830cc444193f1a5ba9a3",
+    "url": "/static/js/1.830cc444.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dff75a1ffda792b2380226033f28415c",
+    "url": "/static/media/sw-bg.dff75a1f.jpg"
+  },
+  {
+    "revision": "2ee343f57f140a923b6c693290c244c7",
+    "url": "/index.html"
+  }
+];
